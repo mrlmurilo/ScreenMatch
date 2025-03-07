a9c27255d0c90da101faee08a1a8d99282f804d8
Murilo <murilomargonar@gmail.com>
2025-03-07 17:37:09 -0300

    feat/Criando dados da temporada

--- src/main/java/br/com/murilo/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/murilo/screenmatch/ScreenmatchApplication.java
@@ -2,6 +2,7 @@ package br.com.murilo.screenmatch;
 
 import br.com.murilo.screenmatch.model.DadosEpisodio;
 import br.com.murilo.screenmatch.model.DadosSerie;
+import br.com.murilo.screenmatch.model.DadosTemporada;
 import br.com.murilo.screenmatch.service.ConsumoAPI;
 import br.com.murilo.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -25,13 +26,22 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
         final var consumoAPI = new ConsumoAPI();
         Scanner teclado = new Scanner(System.in);
-        String serie = teclado.nextLine();
+//        String serie = teclado.nextLine();
+        String serie = "Dexter";
         var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=" + serie + "&apikey=" + apiKey);
         ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados.toString());
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie.toString());
         json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=" + apiKey);
         DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
         System.out.println(dadosEpisodio.toString());
+        json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Dexter&season=4&apikey=" + apiKey);
+        DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+        System.out.println("Dados Temporada");
+//        System.out.println(dadosTemporada.toString());
+        for (DadosEpisodio ep : dadosTemporada.episodios()){
+            System.out.println(ep.toString());
+        }
+
     }
 }
