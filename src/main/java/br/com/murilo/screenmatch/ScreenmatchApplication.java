3cc5079a27d36cc237874127470ecd999d807d11
Murilo <murilomargonar@gmail.com>
2025-03-07 17:28:29 -0300

    feat/Criando dados da do episódio

--- src/main/java/br/com/murilo/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/murilo/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,14 @@
 package br.com.murilo.screenmatch;
 
+import br.com.murilo.screenmatch.model.DadosEpisodio;
 import br.com.murilo.screenmatch.model.DadosSerie;
 import br.com.murilo.screenmatch.service.ConsumoAPI;
 import br.com.murilo.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,9 +24,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
     public void run(String... args) {
 
         final var consumoAPI = new ConsumoAPI();
-        var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Dexter&apikey=a8174a12");
+        Scanner teclado = new Scanner(System.in);
+        String serie = teclado.nextLine();
+        var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=" + serie + "&apikey=" + apiKey);
         ConverteDados conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados.toString());
+        json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=" + apiKey);
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println(dadosEpisodio.toString());
     }
 }
