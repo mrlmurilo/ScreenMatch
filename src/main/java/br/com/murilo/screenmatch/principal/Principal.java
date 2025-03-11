e7c82b086451ec69fffbe310c259e844e7d63241
Murilo <murilomargonar@gmail.com>
2025-03-11 20:16:19 -0300

    feat/analisando média de avaliação de uma temporada

--- src/main/java/br/com/murilo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/murilo/screenmatch/principal/Principal.java
@@ -52,19 +52,18 @@ public class Principal {
                 .flatMap(t -> t.episodios().stream()
                         .map(d -> new Episodio(t.numero(), d))).collect(Collectors.toList());
 
-        System.out.println("Digite o título do ep: ");
-        var trechoTitulo = teclado.nextLine();
+//        System.out.println("Digite o título do ep: ");
+//        var trechoTitulo = teclado.nextLine();
 
-        Optional<Episodio> episodio = episodios.stream()
-                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
-                .findFirst();
-
-        if (episodio.isPresent()) {
-            System.out.println(episodio.get().getTitulo());
-        } else {
-            System.out.println("Episódio não encontrado");
-        }
-
+//        Optional<Episodio> episodio = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//
+//        if (episodio.isPresent()) {
+//            System.out.println(episodio.get().getTitulo());
+//        } else {
+//            System.out.println("Episódio não encontrado");
+//        }
 
 //        System.out.println("A partir de que ano você deseja ver os episódios? ");
 //        var ano = teclado.nextInt();
@@ -76,6 +75,12 @@ public class Principal {
 //        episodios.stream()
 //                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(data))
 //                .forEach(System.out::println);
+
+        Map<Integer, Double> avaliacaoPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getAvaliacao)));
+
+        System.out.println(avaliacaoPorTemporada);
 
     }
 
