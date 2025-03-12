9a7a3fd1dfc7c735c29d9af9845734ea088791d7
Murilo <murilomargonar@gmail.com>
2025-03-11 23:05:37 -0300

    feat/adicionando DoubleSummaryStatistics para calcular média

--- src/main/java/br/com/murilo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/murilo/screenmatch/principal/Principal.java
@@ -39,14 +39,14 @@ public class Principal {
         List<DadosEpisodio> dadosEpisodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream()).toList();
 
-        System.out.println("\nTop 10 episódios melhor avaliados: ");
-        dadosEpisodios.stream()
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .peek(e -> System.out.println("Primeiro Filtro(N/A) " + e))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(10)
-                .map(e -> e.titulo().toUpperCase())
-                .forEach(System.out::println);
+//        System.out.println("\nTop 10 episódios melhor avaliados: ");
+//        dadosEpisodios.stream()
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("Primeiro Filtro(N/A) " + e))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .limit(10)
+//                .map(e -> e.titulo().toUpperCase())
+//                .forEach(System.out::println);
 
         List<Episodio> episodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream()
@@ -76,11 +76,19 @@ public class Principal {
 //                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(data))
 //                .forEach(System.out::println);
 
-        Map<Integer, Double> avaliacaoPorTemporada = episodios.stream()
-                .filter(e -> e.getAvaliacao() > 0.0)
-                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getAvaliacao)));
+//        Map<Integer, Double> avaliacaoPorTemporada = episodios.stream()
+//                .filter(e -> e.getAvaliacao() > 0.0)
+//                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getAvaliacao)));
+//
+//        System.out.println(avaliacaoPorTemporada);
 
-        System.out.println(avaliacaoPorTemporada);
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média:" + est.getAverage());
+        System.out.println("Melhor episódio:" + est.getMax());
+        System.out.println("Pior episódio:" + est.getMin());
 
     }
 
